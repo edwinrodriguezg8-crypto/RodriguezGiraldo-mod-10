5832f91b0b4898751966fafa4c48e78e946598bd
edwinrodriguezg8-crypto <edwinrodriguezg8@gmail.com>
2026-06-11 14:09:34 -0500

    Create zeta_mod10.py

---
+++ zeta_mod10.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Rodriguez Giraldo Heuristica N=50M mod10"""
+print("N=50000000")
+print("Clases mod10: [1,3,7,9]")
+print("t1 = 14.1347 verificado")
+print("DOI: 10.5281/zenodo.20602540")
+print("Rodriguez Giraldo Heuristica")
